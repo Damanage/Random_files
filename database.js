edbf0c0f4eee398668a35185f161f835b0123714
Damanage <derongar@gmail.com>
2017-04-21 19:42:47 +0300

    comm

--- database.js
+++ database.js
@@ -1,18 +1,15 @@
 function MovieAdded(movie, review){
-		this.movie = movie;
-  	this.review = review;
+    this.movie = movie;
+    this.review = review;
 }
 
-moviemaker = function(movie, review){
+movieMaker = function(movie, review){
   films.push(new MovieAdded(movie, review));
 };
 
 var films = [];
 films[0] = new MovieAdded("Toy story 2", "Great story. Mean prospector");
-films[1] = new MovieAdded("Finding Nemo", "Cool animation and funny turtles");
-films[2] = new MovieAdded("The Lion King", "Great songs");
-films.push(new MovieAdded("Family Guy", "Nice"),
-					 new MovieAdded("The Simpsons", "Just a legend!"));
+movieMaker("Family Guy", "Nice");
 
 function printFilm(i){
     console.log(films[i].movie+": "+films[i].review);
@@ -21,7 +18,8 @@ function printFilm(i){
 var find = function(name){
     var contactsLength = films;
     for(var i= 0; i<films.length;i++){
-    	if((films[i].movie).toLowerCase() === (name).toLowerCase()){
+      if((films[i].movie).toLowerCase() === (name).toLowerCase()){
         printFilm(i);
     }
     }};
+find("family guy")
