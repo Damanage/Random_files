945b38fba055ce07a33876df4d55a77e06bee3ea
Damanage <derongar@gmail.com>
2017-04-23 03:43:12 +0300

    ok

---
+++ magicBook.js
@@ -0,0 +1,29 @@
+var autor ={
+	autor: "Great Wizard Joe"
+};
+
+var Fire = function(){
+  this.discipline = "Fire";
+  this.description = "Fire is destruction magic";
+  this.spells = {};
+} 
+Fire.prototype = autor;
+
+var fireSchool = new Fire();
+
+var Spells = function(name, sD){
+	this.spell = name;
+  this.spellDescription = sD;
+};
+Spells.prototype = fireSchool;
+
+fireSchool.spells.firstSpell = new Spells("Fireball", "Classic spell of fire magic school");
+fireSchool.spells.secondSpell = new Spells("Pyroblast","Giant fire boulder");
+
+schoolReview = function(){
+	console.log("Welcome to the school of  "+(fireSchool.discipline.toLowerCase()) +" magic! "+fireSchool.description );
+  for(key in fireSchool.spells){
+  	console.log(fireSchool.spells[key])
+  }};
+  
+schoolReview()
